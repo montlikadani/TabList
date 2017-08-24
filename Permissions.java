b5e5408b00db5f388c8c6ab8b917516b16672547
montlikadani <montlikada@gmail.com>
2017-08-24 12:10:43 +0200

    Add files via upload

---
+++ Permissions.java
@@ -0,0 +1,15 @@
+package hu.montlikadani.TabList;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+	
+	public Permission perm = new Permission("tablist.cmd.nocooldown");
+	public Permission perm1 = new Permission("tablist.plugininfo");
+	public Permission perm2 = new Permission("tablist.help");
+	public Permission perm3 = new Permission("tablist.reload");
+	public Permission perm4 = new Permission("tablist.plugindisable");
+	public Permission perm5 = new Permission("tablist.unnick");
+	public Permission perm6 = new Permission("tablist.nick");
+
+}
