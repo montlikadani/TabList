fdc763c9b6096fed8f5944c22153991e8af82812
montlikadani <montlikada@gmail.com>
2017-08-31 07:57:00 +0200

    Update Permissions.java

--- Permissions.java
+++ Permissions.java
@@ -11,5 +11,7 @@ public class Permissions {
 	public Permission perm4 = new Permission("tablist.plugindisable");
 	public Permission perm5 = new Permission("tablist.unnick");
 	public Permission perm6 = new Permission("tablist.nick");
+	public Permission perm7 = new Permission("tablist.unnick.other");
+	public Permission perm8 = new Permission("tablist.nick.other");
 
 }
