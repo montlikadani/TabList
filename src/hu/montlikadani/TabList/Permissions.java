d9ec5743fc1cd7d5cc1bfb62fb65c94fa5122661
montlikadani <montlikada@gmail.com>
2018-03-06 17:11:11 +0100

    Update Permissions.java

--- src/hu/montlikadani/TabList/Permissions.java
+++ src/hu/montlikadani/TabList/Permissions.java
@@ -4,21 +4,21 @@ import org.bukkit.permissions.Permission;
 
 public class Permissions {
 
-	public Permission perm1 = new Permission("tablist.plugininfo");
-	public Permission perm2 = new Permission("tablist.help");
-	public Permission perm3 = new Permission("tablist.reload");
-	public Permission perm4 = new Permission("tablist.plugindisable");
-	public Permission perm5 = new Permission("tablist.unnick");
-	public Permission perm6 = new Permission("tablist.nick");
-	public Permission perm7 = new Permission("tablist.unnick.other");
-	public Permission perm8 = new Permission("tablist.nick.other");
-	public Permission perm9 = new Permission("tablist.cmd.tabcomplete");
-	public Permission perm10 = new Permission("tablist.unnick.all");
-	public Permission perm11 = new Permission("tablist.get");
-	public Permission perm12 = new Permission("tablist.get.other");
-	public Permission perm13 = new Permission("tablist.seetab");
-	public Permission perm14 = new Permission("tablist.toggle");
-	public Permission perm15 = new Permission("tablist.toggle.off");
-	public Permission perm16 = new Permission("tablist.toggle.on");
+	public static final Permission PINFO = new Permission("tablist.plugininfo");
+	public static final Permission HELP = new Permission("tablist.help");
+	public static final Permission RELOAD = new Permission("tablist.reload");
+	public static final Permission PDISABLE = new Permission("tablist.plugindisable");
+	public static final Permission TABCOMP = new Permission("tablist.cmd.tabcomplete");
+	public static final Permission UNNICK = new Permission("tablist.unnick");
+	public static final Permission NICK = new Permission("tablist.nick");
+	public static final Permission UNICKO = new Permission("tablist.unnick.other");
+	public static final Permission NICKO = new Permission("tablist.nick.other");
+	public static final Permission UNALL = new Permission("tablist.unnick.all");
+	public static final Permission GET = new Permission("tablist.get");
+	public static final Permission GETO = new Permission("tablist.get.other");
+	public static final Permission SEETAB = new Permission("tablist.seetab");
+	public static final Permission TOGGLE = new Permission("tablist.toggle");
+	public static final Permission TOGGLEOFF = new Permission("tablist.toggle.off");
+	public static final Permission TOGGLEON = new Permission("tablist.toggle.on");
 
 }
