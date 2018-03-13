48d24b92821b54817b3017b18f57d20338ccc30e
montlikadani <montlikada@gmail.com>
2018-03-13 17:22:24 +0100

    Removed and added multiple permissions
    - Removed nick permissions.
    - Added tabname permissions.

--- src/hu/montlikadani/TabList/Permissions.java
+++ src/hu/montlikadani/TabList/Permissions.java
@@ -9,16 +9,15 @@ public class Permissions {
 	public static final Permission RELOAD = new Permission("tablist.reload");
 	public static final Permission PDISABLE = new Permission("tablist.plugindisable");
 	public static final Permission TABCOMP = new Permission("tablist.cmd.tabcomplete");
-	public static final Permission UNNICK = new Permission("tablist.unnick");
-	public static final Permission NICK = new Permission("tablist.nick");
-	public static final Permission UNICKO = new Permission("tablist.unnick.other");
-	public static final Permission NICKO = new Permission("tablist.nick.other");
-	public static final Permission UNALL = new Permission("tablist.unnick.all");
 	public static final Permission GET = new Permission("tablist.get");
 	public static final Permission GETO = new Permission("tablist.get.other");
 	public static final Permission SEETAB = new Permission("tablist.seetab");
 	public static final Permission TOGGLE = new Permission("tablist.toggle");
 	public static final Permission TOGGLEOFF = new Permission("tablist.toggle.off");
 	public static final Permission TOGGLEON = new Permission("tablist.toggle.on");
+	public static final Permission TABNAME = new Permission("tablist.tabname");
+	public static final Permission TABNAMEOTHER = new Permission("tablist.tabname.other");
+	public static final Permission RESET = new Permission("tablist.reset");
+	public static final Permission RESETOTHERTAB = new Permission("tablist.reset.other");
 
 }
