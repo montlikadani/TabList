54f81b2a882fa91f91124fe62b602577c749b07a
montlikadani <montlikada@gmail.com>
2020-05-07 10:24:41 +0200

    Fix groups not showing for other players
    - Now if the permission is not set, then the player will not get groups.

--- src/hu/montlikadani/tablist/Sponge/src/tablist/groups/TabGroup.java
+++ src/hu/montlikadani/tablist/Sponge/src/tablist/groups/TabGroup.java
@@ -1,9 +1,7 @@
 package hu.montlikadani.tablist.Sponge.src.tablist.groups;
 
-import java.util.Optional;
-
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.entity.living.player.tab.TabListEntry;
 import org.spongepowered.api.scoreboard.Team;
 import org.spongepowered.api.text.Text;
 
@@ -70,50 +68,42 @@ public class TabGroup implements Cloneable {
 		return priority + groupName;
 	}
 
-	public void setGroup(Player player) {
-		if (!player.hasPermission(permission)) {
+	public void setGroup(final Player player) {
+		if (!player.getSubjectData().getPermissions(player.getActiveContexts()).containsKey(permission)
+				&& !player.hasPermission(permission)) {
 			return;
 		}
 
-		String pref = TabList.get().makeAnim(prefix);
-		String suf = TabList.get().makeAnim(suffix);
+		final String pref = TabList.get().makeAnim(prefix);
+		final String suf = TabList.get().makeAnim(suffix);
+		final String teamName = getFullGroupName();
 
 		Team team = null;
-		if (!TabList.BOARD.getTeam(getFullGroupName()).isPresent()) {
-			team = Team.builder().name(getFullGroupName()).build();
+		if (!TabList.BOARD.getTeam(teamName).isPresent()) {
+			team = Team.builder().name(teamName).build();
 			TabList.BOARD.registerTeam(team);
 		} else {
-			team = TabList.BOARD.getTeam(getFullGroupName()).get();
-		}
-
-		Text name = TabList.get().getVariables().replaceVariables(player, pref + player.getName() + suf);
-
-		org.spongepowered.api.entity.living.player.tab.TabList tablist = player.getTabList();
-		TabListEntry entry = null;
-		Optional<TabListEntry> e = tablist.getEntry(player.getUniqueId());
-		if (e.isPresent()) {
-			entry = e.get();
-			entry.setDisplayName(name);
-		} else {
-			entry = TabListEntry.builder().list(tablist).gameMode(player.gameMode().get()).displayName(name)
-					.profile(player.getProfile()).build();
-		}
-
-		if (!tablist.getEntries().contains(entry)) {
-			tablist.addEntry(entry);
+			team = TabList.BOARD.getTeam(teamName).get();
 		}
 
 		team.addMember(player.getTeamRepresentation());
 
+		final Text name = TabList.get().getVariables().replaceVariables(player, pref + player.getName() + suf);
+
+		Sponge.getServer().getOnlinePlayers().forEach(all -> {
+			all.getTabList().getEntry(player.getUniqueId()).ifPresent(te -> {
+				te.setDisplayName(name);
+			});
+		});
+
 		setScoreboard(player);
 	}
 
-	public void removeGroup(Player player) {
-		Optional<Team> team = TabList.BOARD.getTeam(getFullGroupName());
-		if (team.isPresent()) {
-			team.get().removeMember(player.getTeamRepresentation());
+	public void removeGroup(final Player player) {
+		TabList.BOARD.getTeam(getFullGroupName()).ifPresent(t -> {
+			t.removeMember(player.getTeamRepresentation());
 			setScoreboard(player);
-		}
+		});
 	}
 
 	public void setScoreboard(Player player) {
