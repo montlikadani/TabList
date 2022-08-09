c0545381f404242c91dd433c4c162a4f2db798f5
montlikadani <montlikada@gmail.com>
2022-08-09 11:56:48 +0200

    Only register 1 event

--- bukkit/src/main/java/hu/montlikadani/tablist/listeners/resources/EssAfkStatus.java
+++ bukkit/src/main/java/hu/montlikadani/tablist/listeners/resources/EssAfkStatus.java
@@ -1,12 +1,19 @@
 package hu.montlikadani.tablist.listeners.resources;
 
+import org.bukkit.event.Listener;
+
+import net.ess3.api.events.AfkStatusChangeEvent;
+
 public final class EssAfkStatus extends AfkPlayers {
 
 	public EssAfkStatus(hu.montlikadani.tablist.TabList tl) {
-		tl.getServer().getPluginManager().registerEvents(new org.bukkit.event.Listener() {
+		tl.getServer().getPluginManager().registerEvent(AfkStatusChangeEvent.class, new Listener() {
+		}, org.bukkit.event.EventPriority.NORMAL, new org.bukkit.plugin.EventExecutor() {
 
-			@org.bukkit.event.EventHandler
-			public void onAfkChange(net.ess3.api.events.AfkStatusChangeEvent event) {
+			@Override
+			public void execute(Listener listener, org.bukkit.event.Event e) {
+				AfkStatusChangeEvent event = (AfkStatusChangeEvent) e;
+
 				goAfk(event.getAffected().getBase(), event.getValue());
 			}
 		}, tl);
--- bukkit/src/main/java/hu/montlikadani/tablist/listeners/resources/PurpurAfkStatus.java
+++ bukkit/src/main/java/hu/montlikadani/tablist/listeners/resources/PurpurAfkStatus.java
@@ -1,12 +1,18 @@
 package hu.montlikadani.tablist.listeners.resources;
 
+import org.bukkit.event.Listener;
+import org.purpurmc.purpur.event.PlayerAFKEvent;
+
 public final class PurpurAfkStatus extends AfkPlayers {
 
 	public PurpurAfkStatus(hu.montlikadani.tablist.TabList tl) {
-		tl.getServer().getPluginManager().registerEvents(new org.bukkit.event.Listener() {
+		tl.getServer().getPluginManager().registerEvent(PlayerAFKEvent.class, new Listener() {
+		}, org.bukkit.event.EventPriority.NORMAL, new org.bukkit.plugin.EventExecutor() {
 
-			@org.bukkit.event.EventHandler
-			public void onAfkChange(org.purpurmc.purpur.event.PlayerAFKEvent event) {
+			@Override
+			public void execute(Listener listener, org.bukkit.event.Event e) {
+				PlayerAFKEvent event = (PlayerAFKEvent) e;
+
 				goAfk(event.getPlayer(), event.isGoingAfk());
 			}
 		}, tl);
